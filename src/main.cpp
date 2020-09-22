f8b4bebb21b1780a90a54e722f11ed399d87bfde
mammix2 <mammix2@hotmail.com>
2020-09-22 10:37:55 +0100

    PoW block reward

--- src/main.cpp
+++ src/main.cpp
@@ -1721,14 +1721,10 @@ bool ReadBlockFromDisk(CBlock& block, const CBlockIndex* pindex, const Consensus
 
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
-    int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
-    // Force block reward to zero when right shift is undefined.
-    if (halvings >= 64)
-        return 0;
 
-    CAmount nSubsidy = 50 * COIN;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-    nSubsidy >>= halvings;
+
+    CAmount nSubsidy = 1000 * COIN;
+ 
     return nSubsidy;
 }
 
