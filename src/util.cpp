ce71b2e9ee5d2dd1c45ed907e1f7df145399eedc
mammix2 <mammix2@hotmail.com>
2020-09-23 20:17:47 +0100

    Util data dir.

--- src/util.cpp
+++ src/util.cpp
@@ -455,10 +455,10 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 boost::filesystem::path GetDefaultDataDir()
 {
     namespace fs = boost::filesystem;
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Bitcoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Bitcoin
-    // Mac: ~/Library/Application Support/Bitcoin
-    // Unix: ~/.bitcoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Kobocoin
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Kobocoin
+    // Mac: ~/Library/Application Support/Kobocoin
+    // Unix: ~/.Kobocoin
 #ifdef WIN32
     // Windows
     return GetSpecialFolderPath(CSIDL_APPDATA) / "kobocoin";
