ee60fa0388e6cf732c5aa89782f77f637cd191f2
mammix2 <mammix2@hotmail.com>
2020-10-02 21:43:01 +0100

    comment

--- src/main.cpp
+++ src/main.cpp
@@ -2028,10 +2028,16 @@ bool CBlock::CheckBlock(bool fCheckPOW, bool fCheckMerkleRoot, bool fCheckSig) c
 
     if (IsProofOfStake())
     {
+	//m2:issue, need to stop past block being checked for stake reward on new chain.
         // Coinbase output should be empty if proof-of-stake block
-        if ((vtx[0].vout.size() != 2 || !vtx[0].vout[0].IsEmpty() || !vtx[0].vout[1].IsEmpty() ))
-            return error("CheckBlock() : coinbase output not empty for proof-of-stake block");
+        if (vtx[0].vout.size() != 1 || !vtx[0].vout[0].IsEmpty())
+            return DoS(100, error("CheckBlock() : coinbase output not empty for proof-of-stake block"));
+			
+		
+//        if (vtx[0].vout.size() != 2 || !vtx[0].vout[0].IsEmpty() || !vtx[0].vout[1].IsEmpty() )
+//            return DoS(100, error("CheckBlock() : coinbase output not empty for proof-of-stake block"));
 
+			
         // Second transaction must be coinstake, the rest must not be
         if (vtx.empty() || !vtx[1].IsCoinStake())
             return DoS(100, error("CheckBlock() : second tx is not coinstake"));
