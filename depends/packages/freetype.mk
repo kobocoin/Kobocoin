a77e2f1d820238f0d40407ddcad8395fdfdbed95
mammix2 <mammix2@hotmail.com>
2020-11-05 16:14:13 +0000

    CVE-2020-15999: Update FreeType package

--- depends/packages/freetype.mk
+++ depends/packages/freetype.mk
@@ -1,8 +1,8 @@
 package=freetype
-$(package)_version=2.6.3
+$(package)_version=2.10.4
 $(package)_download_path=http://download.savannah.gnu.org/releases/$(package)
-$(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=371e707aa522acf5b15ce93f11183c725b8ed1ee8546d7b3af549863045863a2
+$(package)_file_name=$(package)-$($(package)_version).tar.xz
+$(package)_sha256_hash=86a854d8905b19698bbc8f23b860bc104246ce4854dcea8e3b0fb21284f75784
 
 define $(package)_set_vars
   $(package)_config_opts=--without-zlib --without-png --disable-static
